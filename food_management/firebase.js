6ee12c0c620bae2b4d4c517ba9579353df7e1c75
Sonu Gupta <sonu72752@gmail.com>
2024-08-11 22:03:13 -0500

    implement firebase

---
+++ food_management/firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore} from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCgLboqeQLUUCjykHUIyeCd3T3zE1zwHcE",
+  authDomain: "food-management-de1e4.firebaseapp.com",
+  projectId: "food-management-de1e4",
+  storageBucket: "food-management-de1e4.appspot.com",
+  messagingSenderId: "415861967846",
+  appId: "1:415861967846:web:89a0080d63fcd1c949a427",
+  measurementId: "G-1LPBFWBKS7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const firestore = getFirestore(app);
+
+export { firestore };
